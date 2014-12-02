2b844e45db0406676457ae7f566c4663945e9337
Bradey Foster <ballislife671@gmail.com>
2014-12-02 14:56:00 -0800

    Created a new table and ran the query

--- controller/create-db.php
+++ controller/create-db.php
@@ -6,7 +6,7 @@
     //checking if connection has a connection error
     if($connection->connect_error){
         //kills program and displays error message
-        die("Error: " . $connection->connect_error);
+        die("<p>Error: " . $connection->connect_error . "</p>");
     }
     //selects the database we need 
     $exists = $connection->select_db($database);
@@ -16,12 +16,25 @@
         $query = $connection->query("CREATE DATABASE $database");
         
         if($query){
-            echo "Creation was a Success: " . $database; 
+            echo "<p>Creation was a Success: " . $database . "</p>"; 
         }
     }
     //if database exists this message will be printed out 
     else {
         echo "Database already exists";
+    }
+    //creates a table to store the blog posts in the database 
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int(11) NOT NULL AUTO_INCREMENT,"
+            . "title varchar(255) NOT NULL,"
+            . "post text NOT NULL,"
+            . "PRIMARY KEY (id))"); 
+    //checks if the table was created or not 
+    if($query){
+        echo "<p>successfully created a table: posts</p>";
+    }
+    else {
+        echo"<p>$connection->error</p>";
     }
     $connection->close();
     
